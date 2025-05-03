54cfb0ed015d656a64659e374f7be43e8442c163
obadabaq <obadabaqleh98@gmail.com>
2025-05-03 20:22:38 +0300

    Fix database connection

--- src/helpers/database/typeorm.config.ts
+++ src/helpers/database/typeorm.config.ts
@@ -7,11 +7,11 @@ import { User } from "src/users/user.entity";
 export const typeOrmConfig: TypeOrmModuleOptions = {
     ssl: false,
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
-    database: "polysy",
+    database: process.env.DATABASE_NAME,
     entities: [User, Admin, Post, Comment],
     synchronize: true
 }
